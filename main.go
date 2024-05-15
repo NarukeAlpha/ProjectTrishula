b1e0bf810ccff3876efce92cc0a5dc6032537a77
NarukeAlpha <bagaao@live.com>
2024-05-14 23:02:34 -0700

    Middle of rework, transitioning from mysql to mongo db, cleaning all 3 modules.  Monitor module is stable, database module is still being transitioned, discord module needs improvements

--- main.go
+++ main.go
@@ -15,35 +15,46 @@ import (
 	"ProjectTrishula/monitorService"
 )
 
-var SetUp struct {
+type SetUp struct {
 	Completed bool `json:"completed"`
 }
-var DiscordS struct {
+
+type Discord struct {
 	GuildID  string `json:"guildID"`
 	BotToken string `json:"botToken"`
-	RemCmd   bool   `json:"remCmd"`
+	RemCmd   bool   `json:"remcmd"`
 }
-var DbKey struct {
+
+type DbKey struct {
 	Url        string `json:"url"`
 	User       string `json:"user"`
 	Password   string `json:"password"`
 	Database   string `json:"database"`
 	Collection string `json:"collection"`
 }
-var Monitor struct {
+
+type Monitor struct {
 	Webhook string `json:"webhook"`
 }
+
+type Data struct {
+	SetUp   SetUp   `json:"setUp"`
+	Discord Discord `json:"discord"`
+	DbKey   DbKey   `json:"dbKey"`
+	Monitor Monitor `json:"monitor"`
+}
+
+var data Data
+
 var mw io.Writer
+
+var datajsonenv = "data.dev.json"
 
 func init() {
 
-	// load json data file
-	// load data into variables
-	// check if data is valid
-	// if data is not valid, start a setup process
-	_, err := os.Stat("data.json")
+	_, err := os.Stat(datajsonenv)
 	if os.IsNotExist(err) {
-		_, err = os.Create("data.json")
+		_, err = os.Create(datajsonenv)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -53,88 +64,53 @@ func init() {
 		log.Fatalln("PoxyList.csv not found, please provide a csv file named ProxyList in the same directory as the exe")
 
 	}
-	file, err := os.Open("data.json")
+	file, err := os.Open(datajsonenv)
 	if err != nil {
 		log.Panicf("Error opening data.json: %v", err)
 
 	}
 	decoder := json.NewDecoder(file)
-	if err = decoder.Decode(&SetUp); err != nil {
+	err = decoder.Decode(&data)
+	if err != nil {
 		log.Panicf("Error decoding data.json: %v", err)
 	}
+	log.Printf("Setup completed: %v", data.DbKey.User)
+	if !data.SetUp.Completed {
 
-	if SetUp.Completed {
+		fmt.Print("Enter Discord GuildID: ")
+		fmt.Scanln(&data.Discord.GuildID)
+		fmt.Print("Enter Discord BotToken: ")
+		fmt.Scanln(&data.Discord.BotToken)
+		fmt.Print("Enter Discord RemCmd: ")
+		fmt.Scanln(&data.Discord.RemCmd)
 
-		err = decoder.Decode(&DiscordS)
-		if err != nil {
-			log.Panicf("Error decoding data.json: %v", err)
+		fmt.Print("Enter DbKey Url: ")
+		fmt.Scanln(&data.DbKey.Url)
+		fmt.Print("Enter DbKey User: ")
+		fmt.Scanln(&data.DbKey.User)
+		fmt.Print("Enter DbKey Password: ")
+		fmt.Scanln(&data.DbKey.Password)
+		fmt.Print("Enter DbKey Database: ")
+		fmt.Scanln(&data.DbKey.Database)
+		fmt.Print("Enter DbKey Collection: ")
+		fmt.Scanln(&data.DbKey.Collection)
 
-		}
-		err = decoder.Decode(&DbKey)
-		if err != nil {
-			log.Panicf("Error decoding data.json: %v", err)
+		fmt.Print("Enter Monitor Webhook: ")
+		fmt.Scanln(&data.Monitor.Webhook)
+		data.SetUp.Completed = true
 
-		}
-		err = decoder.Decode(&Monitor)
-		if err != nil {
-			log.Panicf("Error decoding data.json: %v", err)
-
-		}
-		Core.AssertErrorToNil("failed to close file: %v", file.Close())
-
-	} else {
-		Core.AssertErrorToNil("failed to close file: %v", file.Close())
-
-		// Ask for Discord configuration values
-		fmt.Println("Enter Discord configuration:")
-		fmt.Print("Guild ID: ")
-		fmt.Scanln(&DiscordS.GuildID)
-		fmt.Print("Bot Token: ")
-		fmt.Scanln(&DiscordS.BotToken)
-		fmt.Print("Remove Commands (true/false): ")
-		fmt.Scanln(&DiscordS.RemCmd)
-
-		// Ask for Database configuration values
-		fmt.Println("\nEnter Database configuration:")
-		fmt.Print("Url: ")
-		fmt.Scanln(&DbKey.Url)
-		fmt.Print("User: ")
-		fmt.Scanln(&DbKey.User)
-		fmt.Print("Password: ")
-		fmt.Scanln(&DbKey.Password)
-		fmt.Print("Database: ")
-		fmt.Scanln(&DbKey.Database)
-		fmt.Print("Collection: ")
-		fmt.Scanln(&DbKey.Collection)
-
-		// Ask for Monitor configuration value
-		fmt.Println("\nEnter Monitor configuration:")
-		fmt.Print("Webhook: ")
-		fmt.Scanln(&Monitor.Webhook)
-
-		SetUp.Completed = true
-		file, err = os.OpenFile("data.dev.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+		file, err = os.OpenFile(datajsonenv, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
 		if err != nil {
 			log.Panicf("Error opening data.json: %v", err)
 		}
 
 		encoder := json.NewEncoder(file)
-		if err = encoder.Encode(SetUp); err != nil {
+		if err = encoder.Encode(data); err != nil {
 			log.Panicf("Error encoding data.json: %v", err)
 		}
-		if err = encoder.Encode(DiscordS); err != nil {
-			log.Panicf("Error encoding data.json: %v", err)
-		}
-		if err = encoder.Encode(DbKey); err != nil {
-			log.Panicf("Error encoding data.json: %v", err)
-
-		}
-		if err = encoder.Encode(Monitor); err != nil {
-			log.Panicf("Error encoding data.json: %v", err)
-		}
-		Core.AssertErrorToNil("Error closing data.json: %v", file.Close())
 
 	}
+	Core.AssertErrorToNil("failed to close file: %v", file.Close())
 	_, err = os.Stat("log.txt")
 	if os.IsNotExist(err) {
 		file, err = os.Create("log.txt")
@@ -156,12 +132,21 @@ func init() {
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(2)
-	go dbService.Main(DbKey, &wg)
-	go discordService.Main(DiscordS, &wg)
+	go dbService.Main(data.DbKey, &wg)
+	var discordservicevar struct {
+		GuildID  string `json:"guildID"`
+		BotToken string `json:"botToken"`
+		RemCmd   bool   `json:"remCmd"`
+	}
+	discordservicevar.GuildID = data.Discord.GuildID
+	discordservicevar.BotToken = data.Discord.BotToken
+	discordservicevar.RemCmd = data.Discord.RemCmd
+
+	go discordService.Main(discordservicevar, &wg)
 	wg.Wait()
 
 	log.Println("starting monitor service")
-	go monitorService.Main(Monitor.Webhook)
+	go monitorService.Main(data.Monitor.Webhook)
 	log.Println("All services started")
 
 	stop := make(chan os.Signal, 1)
